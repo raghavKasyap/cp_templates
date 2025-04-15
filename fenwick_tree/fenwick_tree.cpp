98f751c29928d8ced2c2dbcf5cbc2d06fd9a3c54
Raghav Kasyap <raghavk2503@gmail.com>
2025-04-15 23:14:20 +0530

    add variations of fenwick tree

--- fenwick_tree/fenwick_tree.cpp
+++ fenwick_tree/fenwick_tree.cpp
@@ -73,3 +73,79 @@ struct FenwickTreeMin {
       bit[idx] = min(bit[idx], val);
   }
 };
+
+struct FenwickTreeOneBasedIndexing {
+  vector<int> bit; // binary indexed tree
+  int n;
+
+  FenwickTreeOneBasedIndexing(int n) {
+    this->n = n + 1;
+    bit.assign(n + 1, 0);
+  }
+
+  FenwickTreeOneBasedIndexing(vector<int> a)
+      : FenwickTreeOneBasedIndexing(a.size()) {
+    for (size_t i = 0; i < a.size(); i++)
+      add(i, a[i]);
+  }
+
+  int sum(int idx) {
+    int ret = 0;
+    for (++idx; idx > 0; idx -= idx & -idx)
+      ret += bit[idx];
+    return ret;
+  }
+
+  int sum(int l, int r) { return sum(r) - sum(l - 1); }
+
+  void add(int idx, int delta) {
+    for (++idx; idx < n; idx += idx & -idx)
+      bit[idx] += delta;
+  }
+};
+
+// https://cp-algorithms.com/data_structures/fenwick.html#3-range-update-and-range-query
+struct FenwickTreeRangeUpdateRangeQuery {
+  // all indices are 1 based.
+  vector<ll> b1;
+  vector<ll> b2;
+  int n;
+
+  FenwickTreeRangeUpdateRangeQuery(int n) {
+    this->n = n + 1;
+    b1.assign(n + 1, 0);
+    b2.assign(n + 1, 0);
+  }
+
+  FenwickTreeRangeUpdateRangeQuery(vector<int> a)
+      : FenwickTreeRangeUpdateRangeQuery(a.size()) {
+    for (size_t i = 1; i <= a.size(); i++) {
+      range_add(i, i + 1, a[i - 1]);
+    }
+  }
+
+  void range_add(int l, int r, ll x) {
+    add(b1, l, x);
+    add(b1, r + 1, -x);
+    add(b2, l, x * (l - 1));
+    add(b2, r + 1, -x * r);
+  }
+
+  void add(vector<ll> &bit, int idx, int delta) {
+    for (; idx < n; idx += idx & -idx)
+      bit[idx] += delta;
+  }
+
+  ll sum(vector<ll> &bit, int idx) {
+    ll ret = 0;
+    for (; idx > 0; idx -= idx & -idx)
+      ret += bit[idx];
+    return ret;
+  }
+
+  ll point_query(int idx) { return sum(b1, idx); }
+
+  ll prefix_sum(int idx) { return sum(b1, idx) * idx - sum(b2, idx); }
+
+  ll range_sum(int l, int r) { return prefix_sum(r) - prefix_sum(l - 1); }
+};
