43ac7c4d9d84ae69ae79a5466095dd0bb369cc50
Raghav Kasyap <raghavk2503@gmail.com>
2025-04-15 23:14:35 +0530

    rename functions

--- number_theory/binary_exponentiation.cpp
+++ number_theory/binary_exponentiation.cpp
@@ -6,7 +6,10 @@ typedef long long ll;
 typedef unsigned long long ull;
 typedef long double lld;
 
-ll binary_exponentiation(ll a, ll b) {
+const int MOD;
+
+// binary exponentiation
+ll pow(ll a, ll b) {
   ll res = 1;
   while (b > 0) {
     if (b & 1)
@@ -18,14 +21,15 @@ ll binary_exponentiation(ll a, ll b) {
   return res;
 }
 
-ll modulus_binary_exponentiation(ll a, ll b, ll mod) {
-  a %= mod;
+// modulus binary exponentiation
+ll powm(ll a, ll b) {
+  a %= MOD;
   ll res = 1;
   while (b > 0) {
     if (b & 1)
-      res = (res * (a % mod)) % mod;
+      res = (res * (a % MOD)) % MOD;
 
-    a = (a * a) % mod;
+    a = (a * a) % MOD;
     b = b >> 1;
   }
 
